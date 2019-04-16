be21d100c6ac7eec4b1b574670d7e16d681a50af
takahiro.watanabe361@gmail.com <takahiro.watanabe361@gmail.com>
2019-04-16 18:26:27 +0900

    first commit

---
+++ Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	private float hp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Walk(float x){
+		transform.position += Vector3.right * x;
+	}
+
+	public void Jump(Rigidbody2D rb, float power){
+		rb.AddForce(transform.up * power);
+	}
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+	[SerializeField] float _walkSpd, _jumpPower;
+	[SerializeField] KeyCode _jumpKey;
+
+	Rigidbody2D _rb;
+
+	// Use this for initialization
+	void Start () {
+		_rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Walk(Input.GetAxisRaw("Horizontal") * _walkSpd);
+		if(Input.GetKeyDown(_jumpKey)) Jump(_rb, _jumpPower);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		
+	}
+}
